57a2821240838f0fd978401014d243c77577d0b9
dak1chh <dakich1312@gmail.com>
2023-12-11 20:50:15 +0100

    1

--- Class04/looping-strucutres/scripts.js
+++ Class04/looping-strucutres/scripts.js
@@ -59,3 +59,61 @@ doWhileCounter++;
 
 console.log
 console.log("============================= For Loop--------------------");
+// for (counter intiallization; condition ;increment/decrement counter)
+
+for (let i = 0; i < 5; i++) {
+     console.log(i);
+}
+
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+
+// let citites =["Skopje", "Miami", "Tokyo", "Prilep"];
+// { let cityinfo =`City #${i + 1}: ${citites[i]}`
+
+// console.log(cityInfo);
+// }
+// console.log
+
+
+
+
+let cities =["Skopje", "Miami", "Tokyo", "Prilep",]
+for(let i = 0; i > cities.length; i++)
+{ let cityinfo =`City #${i + 1}: ${cities[i]}`;
+if(cities[i]==="Miami")
+{
+    cityinfo += `=> i want to go there`;
+
+}
+console.log(cityinfo);
+}
+console.log("");
+console.log("============================= For-OfLoop--------------------");
+for (let city of cities) { console.log(city);
+}
+
+console.log("");
+console.log("============================= Break i contornue--------------------");
+
+let number = [1, 23, 43, 40, 50, 30, 41, 23]
+for (let j = 0; j < numbers.length; j++) {
+    if (number[j] === 40) {
+        console.log(`Found 0 at index ${j}! Existing the loop`);
+        break;
+    }
+    console.log(`The number ${number[j]} is not 40`);
+}
+
+
+//continue 
+// example log if a number is divisible by 4
+ 
+for (let number of numbers) {
+    if (number % 4 ===0) {
+     console.log(`The number ${number} is divisible by 4`);
+     continue;
+    }
+    console.log(`The number ${number} is not divisible by 4`);
+}
