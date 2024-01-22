d036a416170f7021ab4b4f39a3c9d46ea63ff37b
dak1chh <dakich1312@gmail.com>
2024-01-22 20:25:47 +0100

    1

---
+++ class15/scripts.js
@@ -0,0 +1,42 @@
+let btnCharacters = document.getElementById("btnCharacters");
+let btnShips =documenter.getElementById("btnShips");
+
+let buildTable = (param1, param2, param3, param4, param5, param6) =>
+{ 
+    return
+`<table>
+    <tr>
+        <td>Name</td>
+        <td>${param1}</td>
+    </tr>
+    <tr>
+        <td>Height</td>
+        <td>${param2}</td>
+    </tr>
+    <tr>
+        <td>Mass</td>
+        <td>${param3}</td>
+    </tr>
+    <tr>
+        <td>Gender</td>
+        <td>${param4}</td>
+    </tr>
+    <tr>
+        <td>Birthyear</td>
+        <td>${param5}</td>
+    </tr>
+    <tr>
+        <td>Appearance</td>
+        <td>${param6}</td>
+    </tr>
+</table>`
+}
+
+btnCharacters.addEventListener("click", () => 
+{
+function carachterApi("https://swapi.dev/api/people/?page=1",)
+});
+btnShips.addEventListener("click", () =>
+{
+
+});
